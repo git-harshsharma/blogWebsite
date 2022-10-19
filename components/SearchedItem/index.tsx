2ab80bed725271e85eea929f983eeaa33b5a5400
git-harshsharma <harshsharma@geekyants.com>
2022-10-19 17:20:07 +0530

    Link changes

--- components/SearchedItem/index.tsx
+++ components/SearchedItem/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Link from "next/Link";
+import Link from "next/link";
 
 import { BlogsStructure } from "../../utils/interfaces";
 import { BASE_URL, ROUTES } from "../../utils/Constants";
--- pages/blog/category/[category].tsx
+++ pages/blog/category/[category].tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Link from "next/Link";
+import Link from "next/link";
 
 import HeaderComponent from "../../../components/HeaderComponent";
 import PageHeading from "../../../components/PageHeading";
